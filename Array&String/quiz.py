6f797fffa4c73f8ea7b6f8507deae477485cdb7d
dumbled00r <82684246+dumbled00r@users.noreply.github.com>
2025-05-05 13:22:41 +0700

    first commit

---
+++ Array&String/quiz.py
@@ -0,0 +1,6 @@
+'''Q1: Given nums = [5, 2, 3, 1, 6], the prefix sum would be:'''
+# nums = [5, 2, 3, 1, 6] # result should be [5, 7, 10, 11, 17]
+# res_sum = [nums[0]]
+# for i in range(1, len(nums)):
+#     res_sum.append(nums[i] + res_sum[-1])
+
